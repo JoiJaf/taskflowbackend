2bf99f791f10bd51307f2d4463e72cd5dd1f07d4
AsuncionPD <asuncion.pena@ucr.ac.cr>
2024-06-23 18:13:40 -0600

    Added restrictions to Login method in UserSpecificController
    Se agregarón restricciones de ingreso para el nombre de usuario y contraseña, en el método de Login del controlador de usuarios especificos

--- app/Http/Controllers/UserSpecificController.php
+++ app/Http/Controllers/UserSpecificController.php
@@ -16,34 +16,44 @@ class UserSpecificController extends Controller
 
     public function SingIn(Request $request){
 
-    try{
+        try{
 
-        //Permite obtener datos específicos enviados por POST
-        $data = $request->only(['username', 'emailAddress', 'password']);
+            //Permite obtener datos específicos enviados por POST
+            $data = $request->only(['username', 'emailAddress', 'password']);
+            
+            $user = new UserSpecific();
+            $user->email = $data['emailAddress'];
+            $user->user_name = $data['username'];
+            $user->password = bcrypt($data['password']);
+            $user->user_level_id = 1;
+            $user->save();
+    
+            return response()->json(['mensaje' => 'Usuario creado exitosamente','usuario' => $user], 201); // Código de estado 201: Creado
+    
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
 
-        $user = new UserSpecific();
-        $user->email = $data['emailAddress'];
-        $user->user_name = $data['username'];
-        $user->password = bcrypt($data['password']);
-        $user->user_level_id = 1;
-        $user->save();
-
-        return response()->json(['mensaje' => 'Usuario creado exitosamente','usuario' => $user], 201); // Código de estado 201: Creado
-
-    } catch (\Exception $e) {
-        return response()->json(['error' => $e->getMessage()], 500);
     }
-    }
 
-    public function Login(Request $request){
-
+    public function Login(Request $request)
+    
+    {
         //Permite obtener datos específicos enviados por POST
         $data = $request->only(['username', 'password']);
 
-        UserSpecific::where('user_name', $data['username'])->firstOrFail();
+        $user = UserSpecific::where('user_name', $data['username'])->first();
 
-        return response()->json(['mensaje' => 'Usuario logueado exitosamente'], 200); // Código de estado 200: OK
+        if (!$user) {
+            return response()->json(['error' => 'Usuario no encontrado'], 401);
+        }
+
+        if (!\Hash::check($data['password'], $user->password)) {
+            return response()->json(['error' => 'Contraseña incorrecta'], 401);
+        }
+
+        return response()->json(['mensaje' => 'Usuario logueado exitosamente','usuario' => $user], 200); // Código de estado 200: OK
 
     }
-
+    
 }
